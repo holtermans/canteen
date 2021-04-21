55627839bf8076d120a9fea6c6620b6fae12e521
yangkelin <1137523473@qq.com>
2021-04-22 07:04:49 +0800

    适配getUserProfile接口，不再使用getUserInfo接口；小程序登录调整说明https://developers.weixin.qq.com/community/develop/doc/000cacfa20ce88df04cb468bc52801

--- litemall-wx/pages/auth/login/login.js
+++ litemall-wx/pages/auth/login/login.js
@@ -25,6 +25,43 @@ Page({
     // 页面关闭
 
   },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善用户资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res);
+        app.globalData.userInfo = res.userInfo;
+
+        user.checkLogin().catch(() => { //如果没登录，就登录
+          user.loginByWeixin(res.userInfo).then(Rres => {
+            if (Rres.data.token == null) {
+              wx.redirectTo({
+                url: "/pages/auth/register/register"
+              });
+            } else {
+              app.globalData.hasLogin = true;
+              wx.navigateBack({
+                delta: 1
+              })
+            }
+
+          }).catch((err) => {
+            app.globalData.hasLogin = false;
+            util.showErrorToast('微信登录失败');
+          });
+
+        });
+      },
+      fail: (res) => {
+        console.log(res);
+        app.globalData.hasLogin = false;
+        util.showErrorToast('微信登录失败');
+        return;
+      }
+    })
+  },
   wxLogin: function (e) {
     // console.log(e);
     console.log(e);
@@ -42,13 +79,13 @@ Page({
           wx.redirectTo({
             url: "/pages/auth/register/register"
           });
-        }else{
+        } else {
           app.globalData.hasLogin = true;
           wx.navigateBack({
             delta: 1
           })
         }
-       
+
       }).catch((err) => {
         app.globalData.hasLogin = false;
         util.showErrorToast('微信登录失败');
